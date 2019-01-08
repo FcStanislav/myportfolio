c02bba093982564e4f8655921f77f05e3ae4142a
FcStanislav <2001_stas@mail.ru>
2019-01-09 00:33:04 +0600

    Готовый

--- Заказ/Сверстанная страница/main-page/js/script.js
+++ Заказ/Сверстанная страница/main-page/js/script.js
@@ -22,3 +22,38 @@ tabsControlWrapper.addEventListener("click",(event) => {
          break;
    }
 })
+let dopContent = document.getElementsByClassName("two-banner-content"),
+    dopContentActive = document.getElementsByClassName("modal-2-active");
+
+for (let i = 0; i < dopContentActive.length; i++) { // перебирает все элементы active
+   dopContentActive[i].addEventListener("click", () => { // при клике на элемент (галочка) будет происходить следующее
+
+      for (let j = 0; j < dopContent.length; j++) { ///перебирает все элементы dopContent
+         dopContent[j].classList.remove('two-banner-content-active') //и удаляет у всех них класс show ( что бы закрывались блоки когда открывался другой)(смотри сайт поймешь)
+      }
+      dopContent[i].classList.toggle('two-banner-content-active'); // добовляет класс show на выбранный элемент(toggle - переключает, то есть класс, то нет его)
+   })
+}
+
+// active modal
+   let createButtom = document.getElementById("register-button"),
+       createButtomTwo = document.getElementById("register-button-two"),
+       modalOne = document.getElementsByClassName("modal-one")[0],
+       modalOneClose = document.getElementsByClassName("modal-close")[0],
+       modalTwo = document.getElementsByClassName("modal-two")[0],
+       modalTwoClose = document.getElementsByClassName("modal-two-close")[0];
+      
+   createButtom.addEventListener("click", function () {
+      modalOne.classList.add("modal_active");
+   });
+   modalOneClose.addEventListener("click", () => {
+      modalOne.classList.remove("modal_active");
+   });
+// 2 modals
+
+   createButtomTwo.addEventListener("click", function () {
+      modalTwo.classList.add("two-modal_active");
+     });
+   modalTwoClose.addEventListener("click", () => {
+      modalTwo.classList.remove("two-modal_active");
+     });
